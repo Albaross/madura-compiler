21f886004dde01e217aa32d4577bcb5c12b1ff91
Manuel Feilen <manuel.feilen@de.rhenus.com>
2023-12-11 10:07:18 +0100

    refactored Nodes.kt

--- src/commonMain/kotlin/org/maduralang/compiler/Nodes.kt
+++ src/commonMain/kotlin/org/maduralang/compiler/Nodes.kt
@@ -1,71 +1,29 @@
-open class Node { var tag: String
-    constructor(tag: String = "") {
-        this.tag = tag;
-    }
+interface Node
 
-    open fun print(): String {
-        return this.tag;
-    }
-}
+class Module(var defs: List<Node>) : Node {
 
-class Module : Node { var defs: List<Node>
-    constructor(defs: List<Node>) : super("module") {
-        this.defs = defs;
-    }
-
-    override fun print(): String {
-        return "{\"defs\": ${printList(this.defs)}}"
+    override fun toString(): String {
+        return "{\"defs\": $defs}"
     }
 }
 
-class Function : Node { var name: Node; var params: List<Node>; var type: Node?; var body: List<Node>
-    constructor(name: Node, params: List<Node>, type: Node?, body: List<Node>) : super("fun") {
-        this.name = name;
-        this.params = params;
-        this.type = type;
-        this.body = body;
-    }
+class Function(var name: Node, var params: List<Node>, var type: Node?, var body: List<Node>) : Node {
 
-    override fun print(): String {
-        return "{\"name\": \"${this.name.print()}\", \"params\": ${printList(this.params)}, \"type\": \"${if (this.type != null) this.type!!.print() else "None"}\", \"body\": ${
-            printList(
-                this.body
-            )
-        }}"
+    override fun toString(): String {
+        return "{\"name\": \"$name\", \"params\": $params, \"type\": \"${if (type != null) type else "None"}\", \"body\": $body}"
     }
 }
 
-class Parameter : Node { var name: Node; var type: Node
-    constructor(name: Node, type: Node) : super() {
-        this.name = name;
-        this.type = type;
-    }
+class Parameter(var name: Node, var type: Node) : Node {
 
-    override fun print(): String {
-        return "{\"name\": \"${this.name.print()}\", \"type\": \"${this.type.print()}\"}"
+    override fun toString(): String {
+        return "{\"name\": \"$name\", \"type\": \"$type\"}"
     }
 }
 
-class Call : Node { var name: Node; var args: List<Node>
-    constructor(name: Node, args: List<Node>) : super() {
-        this.name = name;
-        this.args = args;
+class Call(val name: Node, val args: List<Node>) : Node {
+
+    override fun toString(): String {
+        return "{\"name\": \"$name\", \"args\": $args}"
     }
-
-    override fun print(): String {
-        return "{\"name\": \"${this.name.print()}\", \"args\": ${printList(this.args)}}"
-    }
-}
-
-fun printList(list: List<Node>): String {
-    var builder = StringBuilder();
-    builder.append("[");
-
-    for (i in 0..<list.size) {
-        if (i > 0) builder.append(", ");
-        builder.append(list[i].print());
-    }
-
-    builder.append("]");
-    return builder.toString();
 }
