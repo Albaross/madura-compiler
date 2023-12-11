fc4aafdd2085ed357ecd76539d5fb2ae0cdf99ba
Manuel Feilen <manuel.feilen@de.rhenus.com>
2023-12-11 09:35:09 +0100

    refactored Token

--- src/commonMain/kotlin/org/maduralang/compiler/Token.kt
+++ src/commonMain/kotlin/org/maduralang/compiler/Token.kt
@@ -1,21 +1,15 @@
-const val WHITESPACE = 0;
-const val LINEBREAK = 1;
-const val NAME = 2;
-const val KEYWORD = 3;
-const val NUMBER = 4;
-const val STRING = 5;
-const val SYMBOL = 6;
-const val COMMENT = 7;
-const val META = 8;
-const val INVALID = 9;
+const val WHITESPACE = 0
+const val LINEBREAK = 1
+const val NAME = 2
+const val KEYWORD = 3
+const val NUMBER = 4
+const val STRING = 5
+const val SYMBOL = 6
+const val COMMENT = 7
+const val META = 8
+const val INVALID = 9
 
-class Token { var type: Int; var data: String
-    constructor(type: Int, data: String) {
-        this.type = type;
-        this.data = data;
-    }
+class Token(val type: Int, val data: String) {
 
-    fun print(): String {
-        return this.data;
-    }
+    override fun toString(): String = data
 }
